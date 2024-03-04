9b7e274933df1b682d6aeed9a111aec47c91a6fa
Chigozie Peter <ujahchigozie@gmail.com>
2024-03-04 07:28:44 -0800

    Add files via upload

---
+++ src/Exceptions/CacheException.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Luminova Framework
+ *
+ * @package Luminova
+ * @author Ujah Chigozie Peter
+ * @copyright (c) Nanoblock Technology Ltd
+ * @license See LICENSE file
+ */
+namespace Luminova\Psr\Cache\Exceptions;
+
+use \Psr\Cache\CacheException as CacheExceptionInterface;
+use \Luminova\Exceptions\AppException;
+use \Throwable;
+
+class CacheException extends AppException implements CacheExceptionInterface
+{
+    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Luminova Framework
+ *
+ * @package Luminova
+ * @author Ujah Chigozie Peter
+ * @copyright (c) Nanoblock Technology Ltd
+ * @license See LICENSE file
+ */
+namespace Luminova\Psr\Cache\Exceptions;
+
+use \Luminova\Psr\Cache\Exceptions\CacheException;
+
+class InvalidArgumentException extends CacheException
+{
+}
