05f5b2c8c9de046c8c57e0f5dfcbac8b52272959
Chigozie Peter <ujahchigozie@gmail.com>
2024-10-13 21:55:53 +0800

    Update CacheItem.php

--- src/CacheItem.php
+++ src/CacheItem.php
@@ -16,29 +16,39 @@ use \DateInterval;
 class CacheItem implements CacheItemInterface
 {
     /**
-     * @var string $key cache key
-    */
+     * Cache key.
+     * 
+     * @var string $key
+     */
     private string $key = '';
 
     /**
-     * @var mixed $content cache content
-    */
+     * Cache content.
+     * 
+     * @var mixed $content
+     */
     private mixed $content = null;
 
     /**
-     * @var bool $isHit cache hit
-    */
+     * Cache hit.
+     * 
+     * @var bool $isHit
+     */
     private bool $isHit = false;
 
     /**
-     * @var ?DateTimeInterface $expiration cache expiration
-    */
+     * Cache expiration.
+     * 
+     * @var ?DateTimeInterface $expiration
+     */
     public ?DateTimeInterface $expiration = null;
 
     /**
-     * @var int|DateInterval|null $expireAfter cache expiration after
-    */
-    public int|DateInterval|null $expireAfter = null;
+     * Cache expiration after.
+     * 
+     * @var DateInterval|int|null $expireAfter
+     */
+    public DateInterval|int|null $expireAfter = null;
 
     /**
      * Constructor.
@@ -46,17 +56,17 @@ class CacheItem implements CacheItemInterface
      * @param string $key The cache item key.
      * @param mixed $value The cache item value.
      * @param bool $isHit Indicates if the cache item was a hit.
-    */
+     */
     public function __construct(string $key, mixed $content = null, ?bool $isHit = null)
     {
         $this->key = $key;
         $this->content = $content;
-        $this->isHit = $isHit === null ? $content !== null : $isHit;
+        $this->isHit = ($isHit === null) ? $content !== null : $isHit;
     }
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function getKey(): string
     {
         return $this->key;
@@ -64,7 +74,7 @@ class CacheItem implements CacheItemInterface
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function get(): mixed
     {
         return $this->isHit ? $this->content : null;
@@ -72,7 +82,7 @@ class CacheItem implements CacheItemInterface
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function isHit(): bool
     {
         return $this->isHit;
@@ -80,7 +90,7 @@ class CacheItem implements CacheItemInterface
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function set(mixed $value): static
     {
         $this->content = $value;
@@ -91,7 +101,7 @@ class CacheItem implements CacheItemInterface
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function expiresAt(?DateTimeInterface $expiration): static
     {
         $this->expiration = $expiration;
@@ -102,7 +112,7 @@ class CacheItem implements CacheItemInterface
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function expiresAfter(int|DateInterval|null $time): static
     {
         $this->expireAfter = $time;
